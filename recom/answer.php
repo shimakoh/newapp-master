53b48441581fa6878c73571a56b3425c54576324
嶋田幸之佑 <ec51260@meiji.ac.jp>
2018-09-29 14:55:11 +0900

    臨時処置

--- recom/answer.php
+++ recom/answer.php
@@ -45,16 +45,16 @@ if($_SESSION['recom']['Qc']=='b' && $_SESSION['recom']['Qb']=='b'){
 }elseif($_SESSION['recom']['Qc']=='a' && $_SESSION['recom']['Qb']=='a' &&
                              $_SESSION['recom']['Qwhen']=='a'){
     $sql='SELECT name,j_file FROM actresses WHERE id=10';
-}elseif($_SESSION['recom']['Qa']=='d' && $_SESSION['recom']['Qh']=='a' &&
-                             $_SESSION['recom']['Qwhob']=='c' ||
-        $_SESSION['recom']['Qba']=='c' && $_SESSION['recom']['Qchara']=='c'){
-    $sql='SELECT name,j_file FROM actresses WHERE id=11';
-}elseif($_SESSION['recom']['Qc']=='c' && $_SESSION['recom']['Qha']=='b' ){
-    $sql='SELECT name,j_file FROM actresses WHERE id=12';
-}elseif($_SESSION['recom']['Qc']=='c' $_SESSION['recom']['Qwho']=='a'){
-    $sql='SELECT name,j_file FROM actresses WHERE id=13';
-}elseif($_SESSION['recom']['Qha']=='b' && $_SESSION['recom']['Qsatisfy']=='b'){
-    $sql='SELECT name,j_file FROM actresses WHERE id=14';
+// }elseif($_SESSION['recom']['Qa']=='d' && $_SESSION['recom']['Qh']=='a' &&
+//                              $_SESSION['recom']['Qwhob']=='c' ||
+//         $_SESSION['recom']['Qba']=='c' && $_SESSION['recom']['Qchara']=='c'){
+//     $sql='SELECT name,j_file FROM actresses WHERE id=11';
+// }elseif($_SESSION['recom']['Qc']=='c' && $_SESSION['recom']['Qha']=='b' ){
+//     $sql='SELECT name,j_file FROM actresses WHERE id=12';
+// }elseif($_SESSION['recom']['Qc']=='c' $_SESSION['recom']['Qwho']=='a'){
+//     $sql='SELECT name,j_file FROM actresses WHERE id=13';
+// }elseif($_SESSION['recom']['Qha']=='b' && $_SESSION['recom']['Qsatisfy']=='b'){
+//     $sql='SELECT name,j_file FROM actresses WHERE id=14';
 // }elseif($_SESSION['recom']['Qc']=='a' && $_SESSION['recom']['Qb']=='a'&&
 //                            $_SESSION['recom']['Qchara']=='a'){
 //     $sql='SELECT name,j_file FROM actresses WHERE id=15';
