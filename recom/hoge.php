813c1b10597947a27a11fa002bb2eebea1454385
嶋田幸之佑 <ec51260@meiji.ac.jp>
2018-09-29 15:30:46 +0900

    cloudinary 設定場所移動

--- recom/hoge.php
+++ recom/hoge.php
@@ -3,11 +3,11 @@ require '../vendor/autoload.php';
 require('dbconnect.php');
 
 
-\Cloudinary::config(array(
-    "cloud_name" => "hoqcb5m25",
-    "api_key" => "867285216379377",
-    "api_secret" => "kWv8Z3zei67qmqlOpmtiqLzxL5g"
-));
+// \Cloudinary::config(array(
+//     "cloud_name" => "hoqcb5m25",
+//     "api_key" => "867285216379377",
+//     "api_secret" => "kWv8Z3zei67qmqlOpmtiqLzxL5g"
+// ));
  ?>
 
 <!DOCTYPE html>
@@ -17,6 +17,9 @@ require('dbconnect.php');
   <title>test</title>
 </head>
 <body>
-                  <img src="http://res.cloudinary.com/hoqcb5m25/image/upload/v1538030778/avimg/abemi.jpg" width="80%">
+<header>練習</header>
+<h1>
+   <img src="http://res.cloudinary.com/hoqcb5m25/image/upload/v1538030778/avimg/abemi.jpg" >
+</h1>
 </body>
 </html>
