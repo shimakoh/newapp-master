ffdc919e3e1c1e39c8ddae6650d7d8e805f217e7
嶋田幸之佑 <ec51260@meiji.ac.jp>
2018-09-30 17:29:09 +0900

    画像挿入確認

--- recom/answer.php
+++ recom/answer.php
@@ -122,7 +122,8 @@ echo '</pre>';
                     <?php echo $record['name']; ?>
                 </h2>
 <!--                  <img src="/assets/img/<?php $record['j_file'] ?>" width="80%"> -->
-                  <img src="http://res.cloudinary.com/hoqcb5m25/image/upload/v1538030778/avimg/<?php $record['j_file'] ?>">
+<!--                   <img src="http://res.cloudinary.com/hoqcb5m25/image/upload/v1538030778/avimg/<?php $record['j_file'] ?>"> -->
+                  <img src="http://res.cloudinary.com/hoqcb5m25/image/upload/v1538030778/avimg/abemi.jpg">
             </h1>
         </div>
     </div>
