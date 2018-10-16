93f6b0ef0b313f8b8f7ff2a87ddbe01c61995e54
嶋田幸之佑 <ec51260@meiji.ac.jp>
2018-10-16 14:33:43 +0900

    メタタグ位置変更

--- index.php
+++ index.php
@@ -53,13 +53,13 @@ if (!empty($_POST)) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
     <title>AVメイカー</title>
     <link href="assets/css/bootstrap.css" rel="stylesheet">
     <link href="assets/css/main.css" rel="stylesheet">
     <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
